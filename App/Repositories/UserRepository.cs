57efd32557206f41d3a6a08b1ba8e28fe779a9c8
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-05-28 12:40:12 -0300

    🎨 adding format custom

--- App/Repositories/UserRepository.cs
+++ App/Repositories/UserRepository.cs
@@ -2,29 +2,35 @@ using System.Threading.Tasks;
 using App.Data;
 using App.Models;
 
-namespace App.Repositories {
+namespace App.Repositories
+{
 
-    public interface IUserRepository {
+    public interface IUserRepository
+    {
         void Save (User user);
     }
 
-    public class UserRepository : IUserRepository {
+    public class UserRepository : IUserRepository
+    {
 
         private readonly DataContext context;
 
-        public UserRepository (DataContext context) {
+        public UserRepository (DataContext context)
+        {
             this.context = context;
 
         }
-        public async void Save (User user) {
+        public async void Save (User user)
+        {
 
-            context.User.Add (user);
-            await context.SaveChangesAsync ();
+            context.User.Add(user);
+            await context.SaveChangesAsync();
 
         }
 
-        public async Task<User> Get (int id) {
-            return await context.User.FindAsync (id);
+        public async Task<User> Get (int id)
+        {
+            return await context.User.FindAsync(id);
         }
     }
 
