c303b0251420052a9385e80deee56c6fc7cfa4a6
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-05-28 11:17:41 -0300

    ✨ create user repo methods to save and get

---
+++ App/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using App.Data;
+using App.Models;
+
+namespace App.Repositories {
+
+    public interface IUserRepository {
+        void Save (User user);
+    }
+
+    public class UserRepository : IUserRepository {
+
+        private readonly DataContext context;
+
+        public UserRepository (DataContext context) {
+            this.context = context;
+
+        }
+        public async void Save (User user) {
+
+            context.User.Add (user);
+            await context.SaveChangesAsync ();
+
+        }
+
+        public async Task<User> Get (int id) {
+            return await context.User.FindAsync (id);
+        }
+    }
+
+}
