1b082def759daf51c0412bdef83ba9ef6966218e
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-05-30 18:23:10 -0300

    ✅ create tests for user controller

---
+++ Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,71 @@
+using System.Net.Http;
+using App;
+using App.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System;
+using Tests.Models;
+using Tests.Service;
+
+namespace Tests.Controllers
+{
+
+    [TestFixture]
+    public class UserControllerTests
+    {
+
+        private HttpClient client;
+        private TestUser u;
+        private StringContent content;
+
+        [SetUp]
+        public void SetUp ()
+        {
+            client = new ServerService().GetClient();
+            u = new TestUser { Username = "Iury", Password = "123" };
+            string user = JsonSerializer.Serialize(u);
+            content = new StringContent(user, Encoding.UTF8, "application/json");
+
+        }
+
+        [Test]
+        public async Task Return_201_if_user_is_created ()
+        {
+            var response = await client.PostAsync("http://localhost:5000/users", content);
+            Console.WriteLine(response.Headers);
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Return_200_if_user_is_returned ()
+        {
+            //Enviando Usuario para API
+            await client.PostAsync("http://localhost:5000/users", content);
+
+            var response = await client.GetAsync("http://localhost:5000/users/1");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public async Task check_if_returned_user_is_the_same_sended ()
+        {
+            //Enviando Usuario para API
+            await client.PostAsync("http://localhost:5000/users", content);
+
+            var response = await client.GetAsync("http://localhost:5000/users/1");
+
+            string userTested = JsonSerializer.Serialize(new User { Id = 1, Username = "Iury", Password = "123", LastAccess = null }).ToLower();
+
+            string userReturned = (await response.Content.ReadAsStringAsync()).ToLower();
+            Assert.AreEqual(userTested, userReturned);
+        }
+
+
+    }
+
+}
