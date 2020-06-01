7a68774d21ca68837303b67e493ecdd5d51ffedf
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-06-01 19:06:32 -0300

    🔥 remove unecessary fields of model

---
+++ App/Migrations/20200601214341_remove-field-lastaccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class removefieldlastaccess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_access",
+                table: "users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_access",
+                table: "users",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
