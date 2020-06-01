736684e31e9622d55b764b89fa299d586dcfa882
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-06-01 17:18:34 -0300

    👌 remove namespace unecessary

--- Tests/Controllers/UserControllerTest.cs
+++ Tests/Controllers/UserControllerTest.cs
@@ -1,15 +1,11 @@
 using System.Net.Http;
-using App;
 using App.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
 using NUnit.Framework;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Net;
 using System.Text;
 using System;
-using Tests.Models;
 using Tests.Service;
 
 namespace Tests.Controllers
@@ -20,14 +16,14 @@ namespace Tests.Controllers
     {
 
         private HttpClient client;
-        private TestUser u;
+        private User u;
         private StringContent content;
 
         [SetUp]
         public void SetUp ()
         {
             client = new ServerService().GetClient();
-            u = new TestUser { Username = "Iury", Password = "123" };
+            u = new User { Username = "Iury", Password = "123" };
             string user = JsonSerializer.Serialize(u);
             content = new StringContent(user, Encoding.UTF8, "application/json");
 
